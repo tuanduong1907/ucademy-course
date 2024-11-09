bd9df5ad8d74649e61a1915e365bf90725628393
tuanduong19077 <tuanduong1209@gmail.com>
2024-11-09 09:08:53 +0700

    update code

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,13 @@
 import Sidebar from '@/components/layouts/Sidebar'
+import { auth } from '@clerk/nextjs/server';
 import React from 'react'
 
-const layout = ({children}: {children:React.ReactNode}) => {
+const layout = async ({children}: {children:React.ReactNode}) => {
+  const { userId } = await auth();
+
+  // Debugging: Kiểm tra userId
+  console.log("User ID:", userId);
+
   return (
     <div className="wrapper grid grid-cols-[300px,minmax(0,1fr)] h-screen">
     <Sidebar></Sidebar>
--- src/app/(dashboard)/manage/layout.tsx
+++ src/app/(dashboard)/manage/layout.tsx
@@ -1,15 +1,13 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import { useState } from "react";
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
-  
   const { userId } = await auth();
-  console.log(userId);
+
+  console.log("User ID:", userId);
 
   if (!userId) {
-    // Nếu không có userId, thực hiện redirect
     redirect("/sign-in");
   }
   return <div>{children}</div>;
